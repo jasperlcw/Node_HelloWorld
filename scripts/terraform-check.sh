d78cdefc32b71afa9a1cf1bc0b0d57d1e82b718b
Jasper Wong <jasperlcw2@gmail.com>
2024-10-04 13:31:36 -0700

    modified privilege user check

--- scripts/terraform-check.sh
+++ scripts/terraform-check.sh
@@ -4,18 +4,18 @@ cd ./terraform
 terraform plan -no-color > ./tfplan.txt
 if [ "$?" -ne "0" ]; then
   rm ./tfplan.txt
-  exit 1
+  exit 255
 fi
 
 num_changes=$(cat ./tfplan.txt | grep '# aws' | wc -l)
 
 if [ "$num_changes" -eq "0" ]; then
   echo "Terraform found no changes to the AWS infrastructrure."
-  echo "The terraform plan will be applied."
-  terraform apply -auto-approve
+  rm ./tfplan.txt
+  exit 0
 else
   echo "Terraform plan found changes to the AWS service(s):"
+  cat ./tfplan.txt | grep '# aws'
+  rm ./tfplan.txt
+  exit 1
 fi
-
-cat ./tfplan.txt | grep '# aws'
-rm ./tfplan.txt
