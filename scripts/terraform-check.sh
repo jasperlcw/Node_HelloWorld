fd328fa3805a7201a419874d40c1d2b057544b71
Jasper Wong <jasperlcw2@gmail.com>
2024-10-09 11:54:13 -0700

    adusted terraform check based on requirements

--- scripts/terraform-check.sh
+++ scripts/terraform-check.sh
@@ -8,14 +8,20 @@ if [ "$?" -ne "0" ]; then
 fi
 
 num_changes=$(cat ./tfplan.txt | grep '# aws' | wc -l)
+num_changes_ecs=$(cat ./tfplan.txt | grep '# aws_ecs' | wc -l)
 
 if [ "$num_changes" -eq "0" ]; then
   echo "Terraform found no changes to the AWS infrastructrure."
   rm ./tfplan.txt
   exit 0
+elif [ $(($num_changes-$num_changes_ecs)) == 0 ]; then
+  echo "Terraform plan found changes to the AWS ECS service(s):"
+  cat ./tfplan.txt | grep '# aws_ecs'
+  rm ./tfplan.txt
+  exit 1
 else
   echo "Terraform plan found changes to the AWS service(s):"
   cat ./tfplan.txt | grep '# aws'
   rm ./tfplan.txt
-  exit 1
+  exit 2
 fi
