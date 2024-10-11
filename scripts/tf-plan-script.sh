652eddb38bd63c1572215f81380397bda0fc01fa
Jasper Wong <jasperlcw2@gmail.com>
2024-10-11 11:52:16 -0700

    condensed check and plan logic to single script

--- scripts/tf-plan-script.sh
+++ scripts/tf-plan-script.sh
@@ -7,23 +7,43 @@ fi
 
 image_tag=${1#'image_tag='}
 
+cd ./terraform
+terraform plan -var="IMAGE_TAG=$image_tag" -no-color > ./tfplan.txt
+if [ "$?" -ne "0" ]; then
+  rm ./tfplan.txt
+  echo "There is a syntax error in the terraform configuration. Aborting workflow." 1>&2
+  exit 255
+fi
+
+num_changes=$(cat ./tfplan.txt | grep '# aws' | wc -l)
+num_changes_ecs=$(cat ./tfplan.txt | grep '# aws_ecs' | wc -l)
 status=0
-./terraform-check.sh $image_tag > temp-msg.txt
-status="$?"
+
+if [ "$num_changes" -eq "0" ]; then
+  echo "Terraform found no changes to the AWS infrastructrure." > ../temp-msg.txt
+  status=0
+elif [ $(($num_changes-$num_changes_ecs)) == 0 ]; then
+  echo "Terraform plan found changes to the AWS ECS service(s):" > ../temp-msg.txt
+  cat ./tfplan.txt | grep '# aws_ecs' >> ../temp-msg.txt
+  status=1
+else
+  echo "Terraform plan found changes to the AWS service(s):" > ../temp-msg.txt
+  cat ./tfplan.txt | grep '# aws' >> ../temp-msg.txt
+  status=2
+fi
+rm ./tfplan.txt
+
 if [ "$status" -eq 0 ]; then
   clean_deploy='n/a'
-  echo "No changes were found to the AWS infrastructure. Exiting." >> temp-msg.txt
+  echo "No changes were found to the AWS infrastructure. Exiting." >> ../temp-msg.txt
 elif [ "$status" -eq 1 ]; then
   clean_deploy='true'
-  echo "Deployment is a clean deploy with only changes to ECS detected." >> temp-msg.txt
+  echo "Deployment is a clean deploy with only changes to ECS detected." >> ../temp-msg.txt
 elif [ "$status" -eq 2 ]; then
   clean_deploy='false'
-  echo "Deployment is not a clean deploy (changes other than to ECS were found), so changes will not be applied." >> temp-msg.txt
-elif [ "$status" -eq 255 ]; then
-  clean_deploy='err'
-  echo "There is a syntax error in the terraform configuration. Aborting workflow." >> temp-msg.txt
+  echo "Deployment is not a clean deploy (changes other than to ECS were found), so changes will not be applied." >> ../temp-msg.txt
 else
   clean_deploy='err'
-  echo "Uncaught exit code in the terraform check script. Aborting workflow." >> temp-msg.txt
+  echo "Uncaught exit code in the terraform check script. Aborting workflow." >> ../temp-msg.txt
 fi
 echo $clean_deploy
